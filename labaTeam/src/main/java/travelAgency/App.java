6d542366198bef74320582f8a2955892b1d6cbf0
Juan Pablo Simon <jsimon0301@gmail.com>
2023-02-08 12:39:19 -0300

    Creation of repository. Log4j added.

---
+++ labaTeam/src/main/java/travelAgency/App.java
@@ -0,0 +1,17 @@
+package travelAgency;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static final Logger LOGGER = LogManager.getLogger(App.class);
+    public static void main( String[] args )
+    {
+        LOGGER.info( "Hello Team!" );
+    }
+}
