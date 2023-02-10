b0101717ce13797baeaf92c5bb9ebc3d5f12e674
Matias Aratano <matiasaratano@gmail.com>
2023-02-10 09:21:37 -0300

    connection pool added

--- labaTeam/src/main/java/travelAgency/Main.java
+++ labaTeam/src/main/java/travelAgency/Main.java
@@ -2,16 +2,18 @@ package travelAgency;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import utils.ConnectionPool;
 
 /**
  * Hello world!
- *
  */
-public class Main
-{
+public class Main {
     public static final Logger LOGGER = LogManager.getLogger(Main.class);
-    public static void main( String[] args )
-    {
-        LOGGER.info( "Hello Team!!!! Good Change" );
+
+    public static void main(String[] args) {
+        LOGGER.info("Hello Team!!!! Good Change");
+        ConnectionPool.getInstance();
     }
+
+
 }
