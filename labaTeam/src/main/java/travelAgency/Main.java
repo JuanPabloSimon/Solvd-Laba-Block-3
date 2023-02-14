72a07dc2f38dc7e7b6a11dd70180c39f69fa185c
Matias Aratano <matiasaratano@gmail.com>
2023-02-14 09:29:04 -0300

    unused methods deleted

--- labaTeam/src/main/java/travelAgency/Main.java
+++ labaTeam/src/main/java/travelAgency/Main.java
@@ -1,13 +1,11 @@
 package travelAgency;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import travelAgency.app.Application;
 
 import java.sql.SQLException;
 
 public class Main {
-    public static final Logger LOGGER = LogManager.getLogger(Main.class);
+
 
     public static void main(String[] args) throws SQLException {
         Application app = new Application();
