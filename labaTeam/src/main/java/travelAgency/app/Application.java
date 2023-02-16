7a72333d6241b2719b50a8070f4ab5639ef2571a
agomez <agomez_@live.com.ar>
2023-02-16 11:45:29 -0300

    gitignore updated

--- labaTeam/src/main/java/travelAgency/app/Application.java
+++ labaTeam/src/main/java/travelAgency/app/Application.java
@@ -245,7 +245,7 @@ public class Application {
         // serialize the trip object to JSON string
         String json = om.writeValueAsString(trip);
         // save the JSON string to a file
-        FileWriter fileWriter = new FileWriter("./src/main/resources/json/trip.json");
+        FileWriter fileWriter = new FileWriter("labaTeam/src/main/resources/json/trip.json");
         fileWriter.write(json);
         fileWriter.close();
     }
