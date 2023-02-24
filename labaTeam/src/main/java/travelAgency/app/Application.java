8c9696960d5fda7e164de0a0b0b95e3bdeb7d547
agomez <agomez_@live.com.ar>
2023-02-24 01:27:58 -0300

    code refactored

--- labaTeam/src/main/java/travelAgency/app/Application.java
+++ labaTeam/src/main/java/travelAgency/app/Application.java
@@ -21,7 +21,6 @@ public class Application {
     private List<Airport> destinations;
     private Airport departure;
     private Airport fDestination;
-    private final int MAX_FLIGHT_CHANGES = 2;
     private Scanner scanner = new Scanner(System.in);
 
     public Application() {
@@ -127,7 +126,7 @@ public class Application {
         ArrayList<Trip> completeTrips = new ArrayList<>();
         paths.forEach(path -> {
             ArrayList<Trip> trip = getAirport(path.get(0)).searchRoute(getAirport(path.get(1))); //All direct flights
-            if (path.size() > MAX_FLIGHT_CHANGES) {
+            if (path.size() > 2) {
                 path.stream().filter(stop -> (path.indexOf(stop) != 0) && (path.indexOf(stop) != (path.size() - 1)))
                         .forEach(stop -> {
                             ArrayList<Trip> possibleTripNextPart = getAirport(path.get(path.indexOf(stop)))
--- labaTeam/src/main/java/utils/JsonParser.java
+++ labaTeam/src/main/java/utils/JsonParser.java
@@ -1,21 +1,18 @@
 package utils;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import travelAgency.trip.Trip;
 
-
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class JsonParser {
     private static final Logger LOGGER = LogManager.getLogger(JsonParser.class);
 
-    public static void getTripInJsonFormat(Trip trip, String filePath)  {
+    public static void getTripInJsonFormat(Trip trip, String filePath) {
         try {
             ObjectMapper om = new ObjectMapper();
             om.enable(SerializationFeature.INDENT_OUTPUT);
@@ -25,8 +22,8 @@ public class JsonParser {
             FileWriter fileWriter = new FileWriter(filePath);
             fileWriter.write(json);
             fileWriter.close();
-        } catch (IOException e){
-                LOGGER.error(e.getMessage(), e);
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage(), e);
         }
     }
 
